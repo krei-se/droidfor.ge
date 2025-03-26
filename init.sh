9a33bd0f379419b04e7469a914f4cda4b98beb18
Richard von Krei.se <richard@krei.se>
2025-03-26 13:52:50 +0100

    Initial Apps init.sh

---
+++ init.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#################################################################
+#	IMPORTANT: UPDATE TO LATEST VERSIONS FROM FDROID	#
+#################################################################
+
+# Grab latest Termux, Termux Boot and Magisk App
+curl https://f-droid.org/repo/com.termux.boot_1000.apk -o initialApps/com.termux.boot.apk
+curl https://f-droid.org/repo/com.termux_1020.apk -o initialApps/com.termux.apk
+curl https://f-droid.org/repo/com.topjohnwu.magisk_28100.apk -o initialApps/com.topjohnwu.magisk.apk
+
