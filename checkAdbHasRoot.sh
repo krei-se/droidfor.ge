d45e8ccc752cc3c5525f21f22b0c41474e727463
Richard von Krei.se <richard@krei.se>
2025-03-28 16:36:58 +0100

    clean up and restructure

--- checkAdbHasRoot.sh
+++ checkAdbHasRoot.sh
@@ -10,3 +10,15 @@ else
     echo "Unexpected output from adb root: $OUTPUT"
     exit 1
 fi
+
+# Run adb devices command and capture output
+OUTPUT=$(adb shell 'su -c "echo roottest"' 2>&1)
+
+# Check if output matches expected messages
+if [[ "$OUTPUT" == "roottest" ]]; then
+    echo "✅Shell has su-rights."
+else
+    echo "❌ Unexpected output from adb root: $OUTPUT"
+    echo "❌ Is Magisk installed and allows Shell root access?"
+    exit 1
+fi
