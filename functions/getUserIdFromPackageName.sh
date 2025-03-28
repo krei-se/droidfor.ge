550abba1577528bc8493d9dc4a31efcfd49612ec
Richard von Krei.se <richard@krei.se>
2025-03-28 03:05:04 +0100

    openvpn

---
+++ functions/getUserIdFromPackageName.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+getUserIdFromPackageName() {
+    package_name=$1
+    uid=$(adb shell pm list packages -U | grep "$package_name" | awk -F'uid:' '{print $2}' | tr -d '[:space:]')
+
+    if [[ -n "$uid" ]]; then
+        # Extract the last three digits of the UID
+        app_id=${uid: -3}
+        echo "u0_a$app_id"
+    else
+        echo "Package not found or error retrieving user ID" >&2
+        return 1
+    fi
+}
