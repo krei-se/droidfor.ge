1dda6d095bea45faaf4b8d39673e07187afd470e
Richard von Krei.se <richard@krei.se>
2025-03-30 00:11:47 +0100

    autostart whitelist davx5

--- skeleton/userTasksScripts/400-davx5.sh
+++ skeleton/userTasksScripts/400-davx5.sh
@@ -40,14 +40,18 @@ adb shell pm grant at.bitfire.davdroid at.techbee.jtx.permission.READ
 # allow at.bitfire.davdroid to run in background. this is not an app permission, but deviceidle whitelist
 adb shell dumpsys deviceidle whitelist +at.bitfire.davdroid
 
+
 adb shell monkey -p at.bitfire.davdroid -c android.intent.category.LAUNCHER 1
 
-sleep 2
+sleep 3
 
 adb shell am force-stop at.bitfire.davdroid
 
 # Its FOSS, stop nagging me for donations
 adb shell "grep -q 'time_nextDonationPopup' /data/data/at.bitfire.davdroid/shared_prefs/at.bitfire.davdroid_preferences.xml || sed -i '/<map>/a \ \ \ \ <long name=\"time_nextDonationPopup\" value=\"2100000000000\" />' /data/data/at.bitfire.davdroid/shared_prefs/at.bitfire.davdroid_preferences.xml"
+
+# Sony mostly, but can not hurt lol
+adb shell settings put secure startup_whitelist at.bitfire.davdroid
 
 adb shell monkey -p at.bitfire.davdroid -c android.intent.category.LAUNCHER 1
 
